5f6f5dc9eb06c4b16213854a54d828b5c8ad1e2d
Igor Tuag <igortuag@gmail.com>
2020-09-24 15:22:23 -0300

    Manipulando o dom

---
+++ spa/index.js
@@ -0,0 +1,12 @@
+function carregarBotoes() {
+  console.log("Carregar Botões!");
+  const raiz = document.getElementById("root");
+
+  raiz.innerHTML =
+    "<button onclick='aparecerQuadrado()'>aparecer quadrado vermelho</button>   <button>ajuda 2</button>   <button>about-us 3</button>";
+}
+
+function aparecerQuadrado() {
+  const raiz = document.getElementById("root");
+  raiz.innerHTML += "<div class='quadrado-vermelho'>QUADRADO VERMELHO</div>";
+}
