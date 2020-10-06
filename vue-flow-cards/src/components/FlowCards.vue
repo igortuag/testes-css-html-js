59ba0148645550c1658304ee0bc12cb09305c0de
Igor Tuag <igortuag@gmail.com>
2020-10-06 11:12:38 -0300

    Red card on flow chart

--- vue-flow-cards/src/components/FlowCards.vue
+++ vue-flow-cards/src/components/FlowCards.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
     <h1>Flow Cards Testing</h1>
+
+    <div class="board">
+      <RedCard :position="initialPosition" :cardsChild="cards" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import RedCard from "./RedCard.vue";
+
+export default {
+  name: "FlowCards",
+  data() {
+    return {
+      initialPosition: {
+        y: "20px",
+        x: "20px",
+      },
+      cards: [
+        {
+          id: 1,
+          name: "Teste 1",
+        },
+        {
+          id: 2,
+          name: "Teste 2",
+        },
+        {
+          id: 3,
+          name: "Teste 3",
+        },
+        {
+          id: 4,
+          name: "Teste 4",
+        },
+      ],
+    };
+  },
+  components: {
+    RedCard,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.board {
+  background: #577284;
+  display: block;
+  width: 90vw;
+  height: 80vh;
+  position: relative;
+}
+</style>
---
+++ vue-flow-cards/src/components/RedCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="red-card"
+    :style="{
+      top: position.y,
+      left: position.x,
+    }"
+  >
+    <div class="card">
+      <h2 class="card__title">RED CARD</h2>
+    </div>
+
+    <div v-for="(item, index) in cardsChild" :key="item.id">
+      {{ item.name }} index: {{ index }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RedCard",
+  data() {
+    return {
+      valueTeste: "Teste 0",
+    };
+  },
+  props: {
+    position: {
+      type: Object,
+      default: null,
+    },
+    cardsChild: {
+      type: Array,
+      default: null,
+    },
+  },
+  methods: {
+    mudandoValor(teste) {
+      this.valueTeste = teste;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.red-card {
+  background: #77212e;
+  min-width: 100px;
+  min-height: 100px;
+  position: absolute;
+
+  color: #f3e0be;
+  text-align: center;
+}
+
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__title {
+  }
+}
+</style>
