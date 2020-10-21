0a641460df59755b4e142ff48b6c5885b9037d3f
Igor Tuag <igortuag@gmail.com>
2020-10-21 09:03:11 -0300

    [0805] - rest-e-spread

--- destructuring/index.js
+++ destructuring/index.js
@@ -1,22 +1,20 @@
-// const carro = {
-//   marca: "Fiat",
-//   ano: 2018,
-//   portas: 4,
-// };
+// Extraia o backgroundColor, color e margin do btn
+const btn = document.querySelector("button");
+const btnStyles = getComputedStyle(btn);
+const { background, margin, color } = btnStyles
+console.log(background, margin, color)
 
-// const { marca, ano: teste } = carro;
+// Troque os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript';
+let cursoInativo = "HTML";
+[cursoAtivo, cursoInativo] = [cursoInativo, cursoAtivo]
+console.log(cursoAtivo, cursoInativo)
 
-// console.log(marca);
-// console.log(teste);
-
-// const frutas = ['Banana', 'Uva', 'Morango'];
-
-// const [primeira, segunda, terceira] = frutas;
-
-// console.log(primeira)
-
-function handleKeyboard({key, keyCode}) {
-  console.log(key, keyCode)
-}
-
-document.addEventListener('keydown', handleKeyboard)
+// Corrija o erro abaixoconst
+cachorro = {
+  nome: "Bob",
+  raca: "Labrador",
+  cor: "Amarelo",
+};
+const { cor } = cachorro;
+console.log(cor)
