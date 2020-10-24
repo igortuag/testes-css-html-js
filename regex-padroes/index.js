92808d5919695636f57aad4e11d337d27d6949f6
Igor Tuag <igortuag@gmail.com>
2020-10-24 09:31:44 -0300

    [0903] - regex padroes 2

--- regex-padroes/index.js
+++ regex-padroes/index.js
@@ -22,3 +22,88 @@ const cpfs = [
 for (cpf of cpfs) {
   console.log(cpf, cpf.match(regexCPF));
 }
+
+//CNPJ
+const regexpCNPJ = /\d{2}[-.]?(?:\d{3}[-.]?){2}[-\/]?\d{4}[-.]?\d{2}/g;
+const cnpjs = [
+  "00.000.000/0000-00",
+  "00000000000000",
+  "00-000-000-0000-00",
+  "00.000.000/000000",
+  "00.000.000.000000",
+  "00.000.000.0000.00",
+];
+for (cnpj of cnpjs) {
+  console.log(cnpj, cnpj.match(regexpCNPJ));
+}
+
+//Telefone
+const regexpTELEFONE = /(?:\+?55\s?)?(?:\(?\d{2}\)?[-\s]?)?\d{4,5}[-\s]?\d{4}/g;
+
+const telefones = [
+  "+55 11 98888-8888",
+  "+55 11 98888 8888",
+  "+55 11 988888888",
+  "+55 11988888888",
+  "+5511988888888",
+  "5511988888888",
+  "11 98888-8888",
+  "11 98888 8888",
+  "(11) 98888 8888",
+  "(11) 98888-8888",
+  "11-98888-8888",
+  "11 98888 8888",
+  "11988888888",
+  "11988888888",
+  "988888888",
+  "(11)988888888",
+];
+for (telefone of telefones) {
+  console.log(telefone, telefone.match(regexpTELEFONE));
+}
+
+// Email
+const regexpEMAIL = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+const emails = [
+  "email@email.com",
+  "email@email.com.org",
+  "email-email@email.com",
+  "email_email@email.com",
+  "email.email23@email.com.br",
+  "email.email23@empresa-sua.com.br",
+  "c@contato.cc",
+];
+for (email of emails) {
+  console.log(email, email.match(regexpEMAIL));
+}
+
+// Tag
+const regexpTAG = /<\/?[\w\s="']+\/?>/gi;
+const tags = [
+ '<div>Isso é uma div</div>',
+ '<div class="ativa">Essa está ativa</div>',
+ '<img src="imagem" />',
+ '<img src="imagem">',
+ '<ul class="ativa">',
+ '<li>Essa está ativa</li>',
+ '</ul>'
+];
+for(tag of tags) {
+ console.log(tag, tag.match(regexpTAG));
+}
+
+
+// Tag Apenas o Nome
+const regexpTAG = /(?<=<\/?)[\w]+/gi;
+const tags = [
+ '<div>Isso é uma div</div>',
+ '<div class="ativa">Essa está ativa</div>',
+ '<img src="imagem" />',
+ '<img src="imagem">',
+ '<ul class="ativa">',
+ '<li>Essa está ativa</li>',
+ '</ul>'
+];
+for(tag of tags) {
+ console.log(tag, tag.match(regexpTAG));
+}
